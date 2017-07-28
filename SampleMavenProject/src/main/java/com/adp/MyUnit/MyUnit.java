f16f63ab36e9b35d796955f1e6d520399c8318a8
mogansri <srikanth.moganti@adp.com>
2017-07-28 18:54:08 +0530

    Update MyUnit.java

--- SampleMavenProject/src/main/java/com/adp/MyUnit/MyUnit.java
+++ SampleMavenProject/src/main/java/com/adp/MyUnit/MyUnit.java
@@ -4,4 +4,11 @@ public class MyUnit {
 	public String concatenate(String one, String two){
 		return one + two;
 	}
+	public String concatenate2(String one, String two){
+		return one + two;
+	}
+	public String concatenate3(String one, String two){
+		return one + two;
+	}
+
 }
